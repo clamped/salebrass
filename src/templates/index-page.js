2bb3d10dd7e3574390500e364a128f948eb2f1e5
Phil Clamp <philipclamp@rideways.com>
2019-06-11 18:50:55 +0100

    Add heading and description for intro

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -68,6 +68,14 @@ export const IndexPageTemplate = ({
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div className="content">
+                <div className="columns">
+                  <div className="column is-12">
+                    <h3 className="has-text-weight-semibold is-size-2">
+                      {intro.heading}
+                    </h3>
+                    <p>{intro.description}</p>
+                  </div>
+                </div>
                 <Features gridItems={intro.blurbs} />
               </div>
             </div>
