817ebcfcb86b7857a463d735d88c95be119aabb2
Philip Clamp <phil@clamped.me.uk>
2019-06-16 14:24:49 +0100

    Fix vacancies

--- src/components/Vacancies.js
+++ src/components/Vacancies.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Vacancies = ({ vacancyItems }) => (
-  <div className="columns is-multiline">
-    {vacancyItems.map(item => (
-      <div className="column is-6">
-        <section className="section">
-          <h3>{item.position}</h3>
-          <h4>{item.date}</h4>
-          <p>{item.description}</p>
-        </section>
-      </div>
-    ))}
-  </div>
-)
+const Vacancies = ({ vacancyItems }) => {
+  if (!vacancyItems) {
+    vacancyItems = [];
+  }
+
+  return (
+    <div className="columns is-multiline">
+      {vacancyItems.map(item => (
+        <div className="column is-6">
+          <section className="section">
+            <h3>{item.position}</h3>
+            <h4>{item.date}</h4>
+            <p>{item.description}</p>
+          </section>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 Vacancies.propTypes = {
     vacancyItems: PropTypes.arrayOf(
